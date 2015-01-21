000197ebe6be15c6542c358d7c3fdadd3d168b1b
Hashisx <rmiller805@gmail.com>
2015-01-21 11:18:04 -0800

    acaca

--- blog/classes/class.user.php
+++ blog/classes/class.user.php
@@ -18,7 +18,7 @@ class User{
 	public function create_hash($value)
 	{
 	    return $hash = crypt($value, '$2a$12
-.substr(str_replace('+', '.', base64_encode(sha1(microtime(true), true))), 0, 22));
+.substr(str_replace("+", ".", base64_encode(sha1(microtime(true), true))), 0, 22));
 	}
 
 	private function verify_hash($password,$hash)
--- blog/index.php
+++ blog/index.php
@@ -1,3 +1,11 @@
+
+
+<?php ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+?>
+
+
 <?php require('includes/config.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
