09b99beabcced6a32d85e6595b2dc72652edf12a
Hashisx <rmiller805@gmail.com>
2015-01-21 10:47:53 -0800

    second

--- blog/index.php
+++ blog/index.php
@@ -1,40 +1,41 @@
 <?php require('includes/config.php'); ?>
-
 <!DOCTYPE html>
-<html Lang="en">
+<html lang="en">
 <head>
-	<meta charset='utf-8'>
-	<title>Rod's Blog</title>
-	<link rel="stylesheets" href="style/normal.css">
-	<link rel="stylesheet" href="style/main.css">
+    <meta charset="utf-8">
+    <title>Blog</title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/main.css">
 </head>
 <body>
 
 	<div id="wrapper">
+
 		<h1>Blog</h1>
 		<hr />
-		
-<?php 
-	try{
-		
-		$stmt = $db->query('SELECT postID, postTitle, postDedc, PostDate FROM blog_posts ORDER by postID DESC');
-		while($row = $stmt->fetch()){
-		
-		
-			echo '<div>';
-				echo '<h1><a href="viewpost.php?id='.$row['postTitle'].'</a><h1>
-				echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-				echo '<p>'.$row['postDesc']."</p>';
-				echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';
-			echo '</div>';
+
+		<?php
+			try {
+
+				$stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+				while($row = $stmt->fetch()){
+					
+					echo '<div>';
+						echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+						echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+						echo '<p>'.$row['postDesc'].'</p>';				
+						echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';				
+					echo '</div>';
+
+				}
+
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
 			}
-		} catch(PDOException $e) {
-		echo $e->getMessage();
-		
-		}
-?>
+		?>
 
 	</div>
-	
+
+
 </body>
 </html>
